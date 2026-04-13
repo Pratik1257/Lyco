2882a2bee106264aa863d303a31c7d94e5f8eed4
Pratik Parmar <parmarpratik9642@outlook.com>
2026-04-13 17:58:39 +0530

    Switched base from /v2/ to /

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,12 +3,15 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/v2/',
+  base: '/',
   plugins: [react(), tailwindcss()],
   server: {
     port: 5173,
   },
   optimizeDeps: {
     include: ['react-is', 'recharts'],
+  },
+  build: {
+    chunkSizeWarningLimit: 1000,
   },
 })
